3140a14a0bf910eea584c0357577525f9bf32a5d
Murillo Borges <murillo.borges@luizalabs.com>
2022-02-15 17:26:57 -0300

    🚧 architecture tweaks

--- apps/authentication/src/authentication.module.ts
+++ apps/authentication/src/authentication.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './services/authentication.service';
 import { AuthenticationController } from './controllers/authentication.controller';
-import { SharedModule } from '../../shared/src/shared.module';
 import HealthModule from './healthcheck.module';
 
 @Module({
-    imports: [SharedModule, HealthModule],
+    imports: [HealthModule],
     controllers: [AuthenticationController],
     providers: [AuthenticationService],
 })
-export class AuthenticationModule {
-
-}
+export class AuthenticationModule {}
